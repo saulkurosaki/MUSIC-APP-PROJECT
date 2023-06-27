321c66a4044bfe2cf73fcd7ec7559936818bc84f
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-27 16:03:57 -0600

    ArroundYou.jsx Bug Fixed

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -13,7 +13,7 @@ const CountryTracks = () => {
 
   useEffect(() => {
     axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=at_ZUOrBHSViEXMZlJ1RQ7zGhimTjCMB`)
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
